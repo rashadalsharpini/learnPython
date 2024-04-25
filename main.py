3723b8833c0dbc30708ec185d6f5f67e4322a6e1
rashad <rashadalsharpini@gmail.com>
2024-04-25 16:27:22 +0200

    difference and difference_update

--- main.py
+++ main.py
@@ -21,75 +21,90 @@ print(s[::-1])
 firstName, lastName = splitString(s)
 print(firstName, lastName)
 print(firstName[::-1], lastName[::-1], end='')
-
+print("="*50)
 # split
 a = input("\nenter the value : ")
 s = a.split()
 print(s)
+print("="*50)
 
 # center
 e = "rashad"
 print(e.center(9))
 print(e.center(9, '-'))
+print("="*50)
 
 # count
 f = "I love python and cpp because cpp is fast and python is easy."
 print(f.count("cpp"))
+print("="*50)
 
 # swapCase
 g = "I love Python and Cpp because Cpp is fast and Python is easy."
 print(g.swapcase())
+print("="*50)
 
 # startSwitch
 h = "I love Python"
 print(h.startswith('I'))
 print(h.startswith('P', 7, 12))
+print("="*50)
 
 # endSwitch
 print(h.startswith('n'))
 print(h.startswith('e', 2, 6))
+print("="*50)
 
 # index
 print(f.index("p"))
 print(f.index("p", 0, 10))
 # print(f.index("p", 0, 5))
+print("="*50)
 
 # find
 print(f.find('p'))
 print(f.find('p', 0, 10))
 print(f.find('p', 0, 5))
+print("="*50)
 
 # rJust
 print(e.rjust(10))
 print(e.rjust(10, '#'))
+print("="*50)
 
 # splitLines
 es = """first line
 second line
 third line"""
 print(es.splitlines())
+print("="*50)
 
 # expandTabs
 g = "Hello\tWorld\tI\tlove\tpython"
 gg = "Hello\tWorld\tI\tLove\tPython"
 print(g.expandtabs(2))
+print("="*50)
 
 # isTitle
 print(g.istitle())
 print(gg.istitle())
+print("="*50)
 
 # isLower isUpper
 print(g.islower())
 print(g.isupper())
+print("="*50)
 
 # isIdentifier
 seven = 'rashadAlsharpini'
 eight = 'rashadAlsharpini100'
 nine = 'Rashad--Alsharpini'
+print("="*50)
 
 print(seven.isidentifier())
 print(eight.isidentifier())
 print(nine.isidentifier())
+print("="*50)
 
 # isAlpha
 print(seven.isalpha())
@@ -98,16 +113,19 @@ print(eight.isalpha())
 # isalnum
 print(seven.isalnum())
 print(eight.isalnum())
+print("="*50)
 
 # replace(oldValue, NewValue, Count)
 test = "Hello One Two Three One One"
 print(test.replace("One", '1'))
 print(test.replace("One", '1', 1))
+print("="*50)
 
 # join(iterable)
 myList = ["rashad", "alsharpini", "abdElnaser"]
 print("-".join(myList))
 
+print("="*50)
 
 # old way
 name = "rashad"
@@ -129,6 +147,7 @@ myMoney = 134134134124124531
 print("my money in the bank is:{:,d}".format(myMoney))
 print("my money in the bank is:{:d}".format(myMoney))
 print(f"my name is {name} and my age is {age:.2f}")
+print("="*50)
 
 myAnotherList = [1, 2, 3, "ras", False]
 # append
@@ -202,3 +221,17 @@ print(mySetThree.pop())
 Test = {1, 2, 3, 4}
 mySetThree.update(Test)
 print(mySetThree)
+print("="*50)
+# difference
+A = {1, 2, 3, 4, "rashad", "alsharpini"}
+B = {1, 2, 3, 5}
+print(A, "first time")
+print(A.difference(B), "second time")  # A - B
+print(A, "third time")
+print("="*50)
+# difference update
+print(A, "first time")
+print(A.difference(B), "second time")  # A - B
+A.difference_update(B)
+print(A, "third time")
+print("="*50)
